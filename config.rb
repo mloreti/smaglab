f92bcab00c159156d79f5360eded8f870e8c05e8
Mickey Loreti <mloreti10@gmail.com>
2016-06-13 14:29:18 -0700

    Update deployment

--- config.rb
+++ config.rb
@@ -42,3 +42,15 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
+activate :deploy do |deploy|
+  deploy.method        = :rsync
+  deploy.host          = 'www.mickeyloreti.com'
+  deploy.path          = '/home/mickeylo/public_html/smaglab.com'
+  deploy.user          = 'mickeylo'
+  # Optional Settings
+  # deploy.user  = 'tvaughan' # no default
+  # deploy.port  = 5309 # ssh port, default: 22
+  # deploy.clean = true # remove orphaned files on remote host, default: false
+  # deploy.flags = '-rltgoDvzO --no-p --del' # add custom flags, default: -avz
+end
